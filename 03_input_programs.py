0197e268d8966ed035e4ae0882f1603880c5a122
M-MubashirAI <mubshirmalik414@gmail.com>
2025-08-04 21:41:45 +0500

    initial input program

---
+++ 03_input_programs.py
@@ -0,0 +1,11 @@
+# age = input()
+# print(age) 
+name_A = input("what is your name?")
+age_A = input("enter age:")
+name_B = input("what is your name?")
+age_B = input("enter the age")
+
+if age_A > age_B:
+    print(name_A, "is older than ",name_B)
+else:
+    print(name_A, "is younger than ", name_B)
