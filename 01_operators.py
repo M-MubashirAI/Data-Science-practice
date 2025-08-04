974abad04196f258b40d22320ea1d8f00d3994e7
M-MubashirAI <mubshirmalik414@gmail.com>
2025-08-04 21:26:56 +0500

    operators

---
+++ 01_operators.py
@@ -0,0 +1,44 @@
+# How to write a string?
+print("we are learning AI 6 month course by codanics plateform")
+print("amaar")# this is a string
+print("""codanics
+course
+""")
+#  operators
+print(3+5)# this is an integer
+print(3.5-2.1) # this is an float number 
+
+
+ 
+a = [2,4,5,4]
+print(type(a))
+print(a)
+a = (3,6,9,8)
+print(type(a))
+b = 5
+c = 5
+print(b==c)
+print(b!=c)
+print(b<c)
+print(b>c)
+print(b<=c)
+print(b>=c)
+#  list
+c = [38,44,66,33]
+d = [34,99,48,68]
+print(c+d)
+
+# Use of variables and tips to use them
+
+# to perform calculations
+# to import the data and name that data
+
+# Rules
+
+# always use small letters
+# do not use space instead use  underscore(_)
+# do not use special characters #@! etc
+# do not use reserved words like if,else,print,elif
+# do not use the number at beggining and middle 
+# have short name
+# use descriptive name
