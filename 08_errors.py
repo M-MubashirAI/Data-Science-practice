7a3ca9a8968ea06536f271d4857ba33936031734
M-MubashirAI <mubshirmalik414@gmail.com>
2025-08-17 07:34:26 +0500

    errors controlled

---
+++ 08_errors.py
@@ -0,0 +1,35 @@
+# identation error
+
+n = 7
+if n>5:
+print("n is positive value")
+
+
+# human error or logical erro
+
+n = 7
+if n>5:
+    print("j is positive value")
+    
+    
+# module error
+
+print(np.sqrt(9))
+
+# syntax error
+# Name error
+# Runtime error
+# index error
+# key error
+# value error
+# type error
+# attribute error
+# import error
+# name error
+#zero divion error(1/0)
+# memory error
+# file error
+# time out error
+# out of memory error
+# out of disk space error
+
